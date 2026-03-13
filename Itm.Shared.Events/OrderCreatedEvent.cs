f8e60fd872ca249b95ee87117383ec0f005f57c9
Daniel Villamizar <daniel8106@hotmail.com>
2026-03-12 22:21:31 -0500

    Configure MassTransit publisher in Order.Api

---
+++ Itm.Shared.Events/OrderCreatedEvent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Itm.Shared.Events;
+
+// Usamos 'record' porque los eventos son hechos historicos e inmutables NO PUEDEN CAMBIAR.
+// Lo que pasó, pasó. No podemos permitir que alguien edite el precio en pleno vuelo.
+
+public record OrderCreatedEvent(
+        Guid OrderId,
+        int ProductId,
+        string CustomerEmail,
+        decimal TotalAmount
+        );
+    
--- Order.Api/Program.cs
+++ Order.Api/Program.cs
@@ -1,11 +1,24 @@
 using Order.Api;
 using System.Net.Http.Json;
+using MassTransit;
+using Itm.Shared.Events;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Servicios básicos y Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// CONFIGURACIÓN DEL PRODUCTOR (MassTransit)
+builder.Services.AddMassTransit(x =>
+{
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        // En un trabajo real, esta URL debe venir de configuración segura (KeyVault / env vars)
+        cfg.Host("amqps://miqffttk:1pscfTN1wGyzJHwe8BTEFMyocp9U-bEp@moose.rmq.cloudamqp.com/miqffttk");
+    });
+});
 
 // Cliente HTTP hacia Inventory.Api (ajusta el puerto si cambia Inventory)
 builder.Services.AddHttpClient("InventoryClient", client =>
@@ -22,11 +35,13 @@ if (app.Environment.IsDevelopment())
 }
 
 // Endpoint de creación de orden con lógica SAGA (reservar stock + compensar si falla pago)
-app.MapPost("/api/orders", async (CreateOrderDto order, IHttpClientFactory factory) =>
+app.MapPost(
+    "/api/orders", 
+    async (CreateOrderDto order, IHttpClientFactory factory, IPublishEndpoint publisher) =>
 {
     var invClient = factory.CreateClient("InventoryClient");
+    // Paso 1: Intentar reservar el stock
 
-    // PASO 1: Intentar reservar Stock (La Acción)
     var reduceResponse = await invClient.PostAsJsonAsync("/api/inventory/reduce", order);
 
     if (!reduceResponse.IsSuccessStatusCode)
@@ -36,33 +51,51 @@ app.MapPost("/api/orders", async (CreateOrderDto order, IHttpClientFactory facto
 
     try
     {
-        // PASO 2: Procesar el Pago (Simulación de Fallo)
+        // Paso2: Procesar el pago (simulado con un random para este ejemplo)
         bool paymentSuccess = new Random().Next(0, 10) > 5;
 
         if (!paymentSuccess)
         {
             throw new InvalidOperationException("Fondos Insuficientes en la Tarjeta");
         }
+        // Supongamos que la venta fue exitosa y ya cobraron.
 
-        return Results.Ok(new { Message = "Orden Creada y Pagada Exitosamente" });
+        var newOrderId = Guid.NewGuid();
+        decimal simlatedTotal = 150000m; // En real viene de Price.Api
+
+        // Ajustar a la firma real de OrderCreatedEvent (sin UserEmail)
+        var eventMessage = new OrderCreatedEvent(
+            OrderId: newOrderId,
+            ProductId: order.ProductId,
+            UserEmail: "estudiante@correo.itm.edu.co", //simulado, en real viene del token de autenticación
+            TotalAmount: simlatedTotal
+        );
+
+        await publisher.Publish(eventMessage);
+        Console.WriteLine($"[SISTEMA] Evento OrderCreated publicado para la orden {newOrderId}");
+        return Results.Ok(new
+        {
+            Message = "Orden creada y Pagada exitosamente",
+            OrderId = newOrderId
+        });
     }
     catch (Exception ex)
     {
-        Console.WriteLine($"[ERROR] Falló el pago: {ex.Message}. Iniciando compensación...");
+        Console.WriteLine($"[ERROR] Falló el pago: {ex.Message}. Iniciando compensación de stock...");
 
-        // INICIO DE LA COMPENSACIÓN (SAGA ROLLBACK)
+     // INCIO DE LA COMPENSACIÓN (SAGA ROLLBACK)
         var compensateResponse = await invClient.PostAsJsonAsync("/api/inventory/release", order);
-
         if (compensateResponse.IsSuccessStatusCode)
         {
-            return Results.Problem("El pago falló. El stock fue devuelto correctamente. Intente de nuevo.");
+            return Results.Problem("El pago falló. El sttock due devuelto correctamente. Intente de nuevo.");
         }
-        else
-        {
-            Console.WriteLine("[CRITICAL] Falló la compensación. Datos inconsistentes.");
-            return Results.Problem("Error crítico del sistema. Contacte soporte.");
-        }
+
+        Console.WriteLine("[CRITICAL] Falló la compensación. Datos inconsistentes.");
+        return Results.Problem("Error crítico del sistema. Contacte soporte.");
     }
 });
 
 app.Run();
+
+
+
